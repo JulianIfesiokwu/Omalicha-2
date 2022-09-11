4e86af7d392bc0f6a9188969350fe6ee89b82f22
Julian Ifesiokwu <julianifesiokwu@gmail.com>
2022-09-11 13:03:03 +0100

    added contact page to project

---
+++ omalicha-2/src/components/Styled/ContactPage.styled.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+const StyledContactPage = styled.div`
+  padding: 3rem;
+`;
+
+export default StyledContactPage;
---
+++ omalicha-2/src/pages/Contactpage/ContactPage.component.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import StyledContactPage from "../../components/Styled/ContactPage.styled";
+
+const ContactPage = () => {
+  return <StyledContactPage>I am the contact page</StyledContactPage>;
+};
+
+export default ContactPage;
