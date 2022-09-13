9c0b412514d7563d6f7004ade708c68b25355bb3
Julian Ifesiokwu <julianifesiokwu@gmail.com>
2022-09-13 22:44:46 +0100

    created styled testimonial component and styling

---
+++ omalicha-2/src/components/Styled/Testimonials.styled.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+const StyledTestimonial = styled.div`
+  padding: 3rem;
+`;
+
+export default StyledTestimonial;
---
+++ omalicha-2/src/components/Testimonials/Testimonials.component.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import StyledTestimonial from "../Styled/Testimonials.styled";
+
+const Testimonials = () => {
+  return (
+    <StyledTestimonial>I am the testimonials component.</StyledTestimonial>
+  );
+};
+
+export default Testimonials;
