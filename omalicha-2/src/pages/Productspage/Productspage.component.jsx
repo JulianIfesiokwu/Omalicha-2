b4c0012a8f24e38b75d392dc0220d07711c6babe
Julian Ifesiokwu <julianifesiokwu@gmail.com>
2022-09-18 18:47:17 +0100

    trying to fix changes

--- omalicha-2/src/pages/Productspage/Productspage.component.jsx
+++ omalicha-2/src/pages/Productspage/Productspage.component.jsx
@@ -4,7 +4,12 @@ import StyledProductsPage from "../../components/Styled/ProductsPage.styled";
 const ProductsPage = () => {
   return (
     <StyledProductsPage>
-      <div className="filter-controls"></div>
+      <div className="filter-controls">
+        <h2 className="filter-title">Categories</h2>
+        <p className="category">Hair</p>
+        <p className="category">Nails</p>
+        <p className="category">Wigs</p>
+      </div>
       <div className="products"></div>
     </StyledProductsPage>
   );
